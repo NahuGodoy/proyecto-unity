f7aa0c89af02527ae8c09797274f83ff0b24c065
Nahuel <nahuelgodoy998@gmail.com>
2026-08-29 01:28:15 -0300

    fix: Arreglo a los pickups items

--- Assets/FPS/Scripts/Gameplay/NetworkUtils.cs
+++ Assets/FPS/Scripts/Gameplay/NetworkUtils.cs
@@ -1,23 +1,25 @@
 using UnityEngine;
 using Photon.Pun;
 
-
 namespace Unity.FPS.Gameplay
 {
     public static class NetworkUtils
     {
         /// <summary>
-        /// Busca y devuelve el PlayerCharacterController del jugador local (o el de Singleplayer/Offline).
+        /// Busca y devuelve el PlayerCharacterController
+        /// del jugador local.
         /// </summary>
         public static PlayerCharacterController GetLocalPlayer()
         {
-            PlayerCharacterController[] players = Object.FindObjectsByType<PlayerCharacterController>(FindObjectsInactive.Exclude);
+            PlayerCharacterController[] players =
+                Object.FindObjectsByType<PlayerCharacterController>(
+                    FindObjectsInactive.Exclude
+                );
 
             foreach (var player in players)
             {
                 PhotonView pv = player.GetComponent<PhotonView>();
 
-                // Si es offline / sin Photon O es nuestro jugador local en red
                 if (pv == null || !PhotonNetwork.IsConnected || pv.IsMine)
                 {
                     return player;
@@ -26,16 +28,27 @@ namespace Unity.FPS.Gameplay
 
             return null;
         }
+
         public static void PickupDestroy(GameObject target)
         {
             if (target == null)
                 return;
 
+            NetworkPickup networkPickup =
+                target.GetComponent<NetworkPickup>();
+
+            if (networkPickup != null)
+            {
+                networkPickup.DestroyPickup();
+                return;
+            }
+
+            // Fallback para objetos que no utilizan NetworkPickup
             PhotonView pickupPV = target.GetComponent<PhotonView>();
 
             if (pickupPV != null && PhotonNetwork.IsConnected)
             {
-                if (pickupPV.IsMine || PhotonNetwork.IsMasterClient)
+                if (PhotonNetwork.IsMasterClient)
                 {
                     PhotonNetwork.Destroy(target);
                 }
